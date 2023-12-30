5eb2d4da1a8d53c4319381be5d80e61deb9be888
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2023-12-29 21:49:35 -0800

    create eventListener to add and remove divs with slider

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 let slider = document.querySelector('.slider');
 let output = document.querySelector('.gridSliderOutput');
 let sliderSize = slider.value;
-output.innerHTML = slider.value + " x " + slider.value;
+output.textContent = slider.value + " x " + slider.value;
 
 slider.oninput = function() {
-    output.innerHTML = this.value + " x " + this.value;
+    output.textContent = this.value + " x " + this.value;
 };
 
 
-let mainGrid = document.querySelector('.mainGrid');
+slider.addEventListener('input', () => {
+    let gridSquare = document.createElement('div');
+        gridSquare.classList.add('squares');
+    let mainGrid = document.querySelector('.mainGrid');
+    let newValue = document.querySelector('.slider').value;
+    let squares = document.querySelector('.squares');
+    
+    if (newValue > sliderSize) {
+        mainGrid.appendChild(gridSquare);
+    } else if (newValue < sliderSize) {
+        mainGrid.removeChild(squares);
+    };
 
-
+    sliderSize = newValue;
+});
