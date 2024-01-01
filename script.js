7eb565163dd4a376b6e09d4c547b771d07ed5528
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-01-01 12:09:07 -0800

    add function to create new divs per slider ratio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let slider = document.querySelector('.slider');
 let output = document.querySelector('.gridSliderOutput');
+let mainGrid = document.querySelector('.mainGrid');
 let sliderSize = slider.value;
 output.textContent = slider.value + " x " + slider.value;
 
@@ -7,19 +8,34 @@ slider.oninput = function() {
     output.textContent = this.value + " x " + this.value;
 };
 
+function createStartGrid() {
+    for (let x = 0; x < (slider.value * slider.value); x++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('squares');
+        mainGrid.appendChild(gridSquare);
+    };
+};
+
+createStartGrid();
+
 
 slider.addEventListener('input', () => {
-    let gridSquare = document.createElement('div');
-        gridSquare.classList.add('squares');
-    let mainGrid = document.querySelector('.mainGrid');
-    let newValue = document.querySelector('.slider').value;
-    let squares = document.querySelector('.squares');
-    
+    let newValue = slider.value;
     if (newValue > sliderSize) {
-        mainGrid.appendChild(gridSquare);
-    } else if (newValue < sliderSize) {
-        mainGrid.removeChild(squares);
+        for (let x = mainGrid.childElementCount +1;
+            x <= (newValue * newValue); x++) {
+            let gridSquare = document.createElement('div');
+            gridSquare.classList.add('squares');
+            mainGrid.appendChild(gridSquare);
+        };
+    } else if (newValue < sliderSize){
+        for (let x = mainGrid.childElementCount -1; 
+            x >= (newValue * newValue); x--) {
+                let squares = document.querySelector('.squares');
+                mainGrid.removeChild(squares);
+            };
     };
-
     sliderSize = newValue;
+    console.log(mainGrid.childElementCount);
 });
+
