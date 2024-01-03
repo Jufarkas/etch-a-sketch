1ba465a01d36a4ab6f1b22c4bfd75ef014172373
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-01-02 16:37:59 -0800

    modify function to properly create dynamic grid based on slider value

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+
+///************** SETUP TO USE ADJUSTABLE SLIDER THAT CHANGES GRID SIZE **************///
+
+
 let slider = document.querySelector('.slider');
 let output = document.querySelector('.gridSliderOutput');
 let mainGrid = document.querySelector('.mainGrid');
+let applyButton = document.querySelector('.applyGridSize');
 let sliderSize = slider.value;
 output.textContent = slider.value + " x " + slider.value;
 
@@ -8,51 +13,89 @@ slider.oninput = function() {
     output.textContent = this.value + " x " + this.value;
 };
 
+
 function createStartGrid() {
     for (let x = 0; x < (slider.value * slider.value); x++) {
         let gridSquare = document.createElement('div');
         gridSquare.classList.add('squares');
         mainGrid.appendChild(gridSquare);
-        document.getElementById('mainGridSize').style.width = sliderSize * 6 + "px";  // IF YOU CHANGE THE .squares PIXEL SIZE IN THE CSS FILE DON'T FORGET TO CHANGE THE VALUE YOU MULTIPLY WITH newValue
+
+        const squareSize = document.querySelectorAll('.squares');
+        squareSize.forEach(squares => {
+            squares.style.width = slider.value * 6 + "px";
+            squares.style.height = slider.value * 6 + "px";
+        });
+
     };
 };
+
 
 createStartGrid();
 
 
+/*************** FUNCTION TO APPLY CSS CHANGES TO DIVS TO FIT GRID ***************/
+
+function resizeGrid() {
+    const squareSize = document.querySelectorAll('.squares');
+                squareSize.forEach(squares => {
+                    squares.style.width = 600 / slider.value + "px";
+                    squares.style.height = 600 / slider.value + "px";
+    });
+}
+
+/******************** EVENT LISTENER FOR SLIDER TO RESIZE GRID ********************/
+
 slider.addEventListener('input', () => {
     let newValue = slider.value;
     if (newValue > sliderSize) {
-        for (let x = mainGrid.childElementCount +1;
-            x <= (newValue * newValue); x++) {
+        for (let x = mainGrid.childElementCount +1; x <= (newValue * newValue); x++){
             let gridSquare = document.createElement('div');
             gridSquare.classList.add('squares');
             mainGrid.appendChild(gridSquare);
+            resizeGrid()
         };
     } else if (newValue < sliderSize){
-        for (let x = mainGrid.childElementCount -1; 
-            x >= (newValue * newValue); x--) {
-                let squares = document.querySelector('.squares');
-                mainGrid.removeChild(squares);
-            };
+        for (let x = mainGrid.childElementCount -1; x >= (newValue * newValue); x--){
+            let squares = document.querySelector('.squares');
+            mainGrid.removeChild(squares);
+            resizeGrid()
+        };
     };
-    document.getElementById('mainGridSize').style.width = newValue * 6 + "px";
-    sliderSize = newValue; // IF YOU CHANGE THE .squares PIXEL SIZE IN THE CSS FILE DON'T FORGET TO CHANGE THE VALUE YOU MULTIPLY WITH newValue
-    console.log(mainGrid.childElementCount);
+    sliderSize = newValue;
 });
 
 
 
+/******************* FUNCTION TO CREATE RESIZABLE GRID WITH SLIDER *******************/
 
 
-/*  
+// slider.addEventListener('input', () => {
+//     let newValue = slider.value;
+//     if (newValue > sliderSize) {
+//         for (let x = mainGrid.childElementCount +1; x <= (newValue * newValue); x++){
+//             let gridSquare = document.createElement('div');
+//             gridSquare.classList.add('squares');
+//             mainGrid.appendChild(gridSquare);
 
-    Make 3 buttons that let you choose between 1px, 2px, 4px
+//             const squareSize = document.querySelectorAll('.squares');
+//                 squareSize.forEach(squares => {
+//                     squares.style.width = 600 / slider.value + "px";
+//                     squares.style.height = 600 / slider.value + "px";
+//                 });
+//         };
+//     } else if (newValue < sliderSize){
+//         for (let x = mainGrid.childElementCount -1; x >= (newValue * newValue); x--){
+//             let squares = document.querySelector('.squares');
+//             mainGrid.removeChild(squares);
 
-    Each button selection should increase the squares pixel size by a multiple of the selection
 
-    (currently set to 6px in CSS file, so 1px = 1*6, 2px = 2*6, 4px = 4*6) 
-    
-    thus making them larger (so fewer squares total) to give the appearance of a changing pen size
+//             const squareSize = document.querySelectorAll('.squares');
+//                 squareSize.forEach(squares => {
+//                     squares.style.width = 600 / slider.value + "px";
+//                     squares.style.height = 600 / slider.value+ "px";
+//                 });
+//        };
+//     };
+//     sliderSize = newValue;
+// });
 
-*/
